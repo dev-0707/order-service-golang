d4aa00c8e9152802d15c40e5db00d8773cfe3215
antonior07 <antonior0409@gmail.com>
2023-05-10 18:42:01 +0200

    add openapi scripts

---
+++ generate-oapi-models.bat
@@ -0,0 +1,20 @@
+@echo off
+for %%I in (.) do set CurrDirName=%%~nxI
+TITLE %CurrDirName%
+set folder=%1
+set model=%2
+set excludedSchemas=%3
+set spec=%4
+rem oapi-codegen --config types.cfg.yaml -o internal\api\dto\%model%-dto.go docs\%spec%.yaml
+rem oapi-codegen generate types -o internal\api\dto\%model%_dto.go -i docs\%spec%.yaml
+rem https://github.com/deepmap/oapi-codegen
+REM oapi-codegen -generate types --exclude-schemas=%excludedSchemas% -o internal\api\controllers\%folder%\%model%_dto.go -package %model% %spec%
+
+md internal\api\controllers\%folder%
+echo "oapi-codegen -generate types --exclude-schemas=%excludedSchemas% -o internal\api\controllers\%folder%\%model%_dto.go -package %model% %spec%"
+
+oapi-codegen -generate types --exclude-schemas=%excludedSchemas% -o internal\api\controllers\%folder%\%model%_dto.go -package %model% %spec%
+
+
+rem ES set excludedSchemas=%
+rem ES generate-oapi-models.bat check check %excludedSchemas% docs\tlp-proximity-channel-eb-service-api.yaml
---
+++ generate-oapi.bat
@@ -0,0 +1,15 @@
+@echo off
+for %%I in (.) do set CurrDirName=%%~nxI
+TITLE %CurrDirName%
+set folder=internal\api\controllers\%1
+set package=%1
+set controller=%2
+set spec=%3
+rem openapi-generator-cli generate -g dynamic-html -g go-gin-server -i docs\petstore-expanded.yaml -o server/ --additional-properties=outputAsLibrary=true,sourceFolder=openapipetstore-expanded.yaml > petstore.gen.go
+
+if not exist %folder% md %folder%
+
+oapi-codegen -generate gin,spec -include-tags=%controller% -package %package% -o %folder%\%controller%_controller.go %spec%
+
+
+rem Es: generate-oapi.bat test-api check docs\tlp-proximity-channel-eb-service-api.yaml
