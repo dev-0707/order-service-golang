2704dacb07d3be3a143230f3c0bafd695f5ac733
dev-0707 <antonior0409@gmail.com>
2024-05-07 16:27:04 +0200

    fix script

--- generate-oapi-models.bat
+++ generate-oapi-models.bat
@@ -3,8 +3,8 @@ for %%I in (.) do set CurrDirName=%%~nxI
 TITLE %CurrDirName%
 set folder=%1
 set model=%2
-set excludedSchemas=%3
-set spec=%4
+rem set excludedSchemas=%3
+set spec=%3
 rem oapi-codegen --config types.cfg.yaml -o internal\api\dto\%model%-dto.go docs\%spec%.yaml
 rem oapi-codegen generate types -o internal\api\dto\%model%_dto.go -i docs\%spec%.yaml
 rem https://github.com/deepmap/oapi-codegen
@@ -13,7 +13,8 @@ REM oapi-codegen -generate types --exclude-schemas=%excludedSchemas% -o internal
 md internal\api\controllers\%folder%
 echo "oapi-codegen -generate types --exclude-schemas=%excludedSchemas% -o internal\api\controllers\%folder%\%model%_dto.go -package %model% %spec%"
 
-oapi-codegen -generate types --exclude-schemas=%excludedSchemas% -o internal\api\controllers\%folder%\%model%_dto.go -package %model% %spec%
+rem oapi-codegen -generate types --exclude-schemas=%excludedSchemas% -o internal\api\controllers\%folder%\%model%_dto.go -package %model% %spec%
+oapi-codegen -generate types -o internal\api\controllers\%folder%\%model%_dto.go -package %model% %spec%
 
 
 rem ES set excludedSchemas=%
